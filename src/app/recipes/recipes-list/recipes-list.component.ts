89868c89ae5745da18729bd50245dec8cabc53da
Pintu Kumar <kumarpintu1713122@gmail.com>
2022-08-13 11:53:18 +0530

    added shopping list

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is simply a test", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6YRrNzGqSouiDJMwVbODrHLpIFhxNc7P5DA&usqp=CAU"),
+    new Recipe("A Test Recipe", "This is simply a test", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6YRrNzGqSouiDJMwVbODrHLpIFhxNc7P5DA&usqp=CAU")
+
+  ]
 
   constructor() { }
 
